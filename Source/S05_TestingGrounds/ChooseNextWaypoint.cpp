72dbf90c56fb4c91e08db15b67f446e46de8d1c4
jsdreamer <jsyow88@yahoo.com>
2019-01-05 22:00:11 +0800

    Composition over Inheritance

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,8 +3,9 @@
 #include "S05_TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "PatrollingGuard.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
 #include "AIController.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
@@ -14,12 +15,17 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 	//UE_LOG(LogTemp, Warning, TEXT("Waypoint index %i"), Index);
 
 	auto AIController = OwnerComp.GetAIOwner();
-	auto PatrollingGuard = Cast<APatrollingGuard>(AIController->GetPawn());
-	auto PatrolPoints = PatrollingGuard->GetPatrolPoints();
-
-	BlackboardComp->SetValueAsObject(Waypoint.SelectedKeyName, PatrolPoints[Index++]);
-
-	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index % PatrolPoints.Num());
+	auto PatrollingGuard = Cast<ATP_ThirdPersonCharacter>(AIController->GetPawn());
+	auto PatrolRoute = PatrollingGuard->FindComponentByClass<UPatrolRoute>();
+	
+	if (!PatrolRoute) { return  EBTNodeResult::Failed; }
+	
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() != 0)
+	{
+		BlackboardComp->SetValueAsObject(Waypoint.SelectedKeyName, PatrolPoints[Index++]);
+		BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index % PatrolPoints.Num());
+	}
 
 	return EBTNodeResult::Succeeded;
 }
